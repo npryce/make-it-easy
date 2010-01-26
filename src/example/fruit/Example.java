d70c9f682555ccfc8edda1594b10296474155f0d
nat.pryce <nat.pryce@37f576d0-09ce-11df-beaf-bb271b2086c6>
2010-01-26 17:28:27 +0000

    improved example

--- src/example/fruit/Example.java
+++ src/example/fruit/Example.java
@@ -9,9 +9,10 @@ import static example.fruit.FruitMakers.*;
 @SuppressWarnings({"UnusedDeclaration"})
 public class Example {
     public static void main(String[] args) {
-        Maker<Apple> ripeApple = an(Apple, with(2, leaves), with(ripeness, 0.9));
-        Maker<Apple> unripeApple = an(Apple, like(ripeApple), with(ripeness, 0.125));
-
+        Maker<Apple> appleWith2Leaves = an(Apple, with(2, leaves));
+        Maker<Apple> ripeApple = an(Apple, like(appleWith2Leaves), with(ripeness, 0.9));
+        Maker<Apple> unripeApple = an(Apple, like(appleWith2Leaves), with(ripeness, 0.125));
+        
         Apple apple1 = make(ripeApple);
         Apple apple2 = make(unripeApple);
         
