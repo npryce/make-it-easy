558d016b9403b64a0726a601a2225a724f185b86
nat.pryce <nat.pryce@37f576d0-09ce-11df-beaf-bb271b2086c6>
2010-01-27 18:58:09 +0000

    simplified the API and deleted some interfaces as a result added example using old-skool test-data builders as a comparison

--- src/com/natpryce/makeiteasy/PropertyValue.java
+++ src/com/natpryce/makeiteasy/PropertyValue.java
@@ -1,15 +1,11 @@
 package com.natpryce.makeiteasy;
 
-public class PropertyValue<T,V> implements PropertyProvider<T> {
-    private final Property<T,V> property;
-    private final V value;
+public class PropertyValue<T,V> {
+    public final Property<T,V> property;
+    public final V value;
     
     public PropertyValue(Property<T,V> property, V value) {
         this.property = property;
         this.value = value;
-    }
-
-    public void providePropertiesTo(PropertyCollector<? extends T> propertyCollector) {
-        propertyCollector.collectPropertyValue(property, value);
     }
 }
