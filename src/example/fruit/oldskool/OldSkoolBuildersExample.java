469a4ab308399979b5dd7fcbfb97cd659ab5057e
nat.pryce <nat.pryce@37f576d0-09ce-11df-beaf-bb271b2086c6>
2010-01-29 17:41:15 +0000

    more examples -- how to deal with collections and generics

--- src/example/fruit/oldskool/OldSkoolBuildersExample.java
+++ src/example/fruit/oldskool/OldSkoolBuildersExample.java
@@ -2,16 +2,13 @@ package example.fruit.oldskool;
 
 import example.fruit.Apple;
 import example.fruit.Banana;
-
-import static com.natpryce.makeiteasy.MakeItEasy.*;
-import static example.fruit.makeiteasy.FruitMakers.*;
 import static example.fruit.oldskool.AppleBuilder.anApple;
 import static example.fruit.oldskool.BananaBuilder.aBanana;
 
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class OldSkoolBuildersExample {
-    public static void main(String[] args) {
+    public static void simpleFruitExample() {
         AppleBuilder appleWith2Leaves = anApple().withLeaves(2);
         AppleBuilder ripeApple = appleWith2Leaves.but().withRipeness(0.9);
         AppleBuilder unripeApple = appleWith2Leaves.but().withRipeness(0.125);
