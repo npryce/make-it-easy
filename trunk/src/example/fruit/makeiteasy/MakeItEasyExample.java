93f0a4fa8ba15a04c7314e8e420537579c113997
nat.pryce <nat.pryce@37f576d0-09ce-11df-beaf-bb271b2086c6>
2010-01-29 14:38:36 +0000

    example

--- trunk/src/example/fruit/makeiteasy/MakeItEasyExample.java
+++ trunk/src/example/fruit/makeiteasy/MakeItEasyExample.java
@@ -1,13 +1,17 @@
 package example.fruit.makeiteasy;
 
+import static com.natpryce.makeiteasy.MakeItEasy.a;
+import static com.natpryce.makeiteasy.MakeItEasy.an;
+import static com.natpryce.makeiteasy.MakeItEasy.make;
+import static com.natpryce.makeiteasy.MakeItEasy.with;
 import com.natpryce.makeiteasy.Maker;
 import example.fruit.Apple;
 import example.fruit.Banana;
-
-import static com.natpryce.makeiteasy.MakeItEasy.*;
 import static example.fruit.makeiteasy.FruitMakers.Apple;
 import static example.fruit.makeiteasy.FruitMakers.Banana;
-import static example.fruit.makeiteasy.FruitMakers.*;
+import static example.fruit.makeiteasy.FruitMakers.curve;
+import static example.fruit.makeiteasy.FruitMakers.leaves;
+import static example.fruit.makeiteasy.FruitMakers.ripeness;
 
 
 @SuppressWarnings({"UnusedDeclaration"})
@@ -16,10 +20,12 @@ public class MakeItEasyExample {
         Maker<Apple> appleWith2Leaves = an(Apple, with(2, leaves));
         Maker<Apple> ripeApple = appleWith2Leaves.but(with(ripeness, 0.9));
         Maker<Apple> unripeApple = appleWith2Leaves.but(with(ripeness, 0.125));
-        
+
         Apple apple1 = make(ripeApple);
         Apple apple2 = make(unripeApple);
-        
+
         Banana defaultBanana = make(a(Banana));
+        Banana straightBanana = make(a(Banana, with(curve, 0.0)));
+        Banana squishyBanana = make(a(Banana, with(ripeness, 1.0)));
     }
 }
