b2992dbba956bc81608bcfa07b43e84e20bb29b4
nat.pryce <nat.pryce@37f576d0-09ce-11df-beaf-bb271b2086c6>
2010-01-30 15:42:20 +0000

    improved examples

--- src/example/fruit/makeiteasy/MakeItEasyExample.java
+++ src/example/fruit/makeiteasy/MakeItEasyExample.java
@@ -1,14 +1,13 @@
 package example.fruit.makeiteasy;
 
 import com.natpryce.makeiteasy.Maker;
-import example.fruit.Apple;
-import example.fruit.AppleCart;
-import example.fruit.Banana;
-import example.fruit.Tree;
+import example.fruit.*;
 
 import static com.natpryce.makeiteasy.MakeItEasy.*;
 import static example.fruit.makeiteasy.AppleCartMaker.AppleCart;
 import static example.fruit.makeiteasy.AppleCartMaker.apples;
+import static example.fruit.makeiteasy.FruitBowlMaker.FruitBowl;
+import static example.fruit.makeiteasy.FruitBowlMaker.contents;
 import static example.fruit.makeiteasy.FruitMakers.*;
 import static example.fruit.makeiteasy.TreeMaker.BananaTree;
 import static example.fruit.makeiteasy.TreeMaker.bananas;
@@ -16,7 +15,7 @@ import static example.fruit.makeiteasy.TreeMaker.bananas;
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class MakeItEasyExample {
-    public static void simpleFruitExample() {
+    public static void howToMakeSimpleObjects() {
         Maker<Apple> appleWith2Leaves = an(Apple, with(2, leaves));
         Maker<Apple> ripeApple = appleWith2Leaves.but(with(ripeness, 0.9));
         Maker<Apple> unripeApple = appleWith2Leaves.but(with(ripeness, 0.125));
@@ -29,15 +28,22 @@ public class MakeItEasyExample {
         Banana squishyBanana = make(a(Banana, with(ripeness, 1.0)));
     }
 
-    public static void strudelExampleIllustratingHowToMakeObjectsWithPropertiesThatAreCollections() {
-        AppleCart strudel = make(a(AppleCart, with(apples, listOf(
+    public static void howToMakeObjectsWithPropertiesThatAreCollections() {
+        AppleCart cart = make(a(AppleCart, with(apples, listOf(
             an(Apple, with(ripeness, 0.5)),
             an(Apple, with(ripeness, 0.35))
+        ))));
+
+        FruitBowl bowl = make(a(FruitBowl, with(contents, listOf(
+                an(Apple, with(2, leaves)),
+                an(Apple, with(3, leaves)),
+                a(Banana, with(ripeness, 0.25)),
+                a(Banana, with(ripeness, 0.99))
         ))));
     }
 
     // Yuck! But Java does not do type inference properly.
-    public static void treeExampleIllustratingHowToMakeGenericObjects() {
+    public static void howToMakeGenericObjects() {
         Tree<Banana> bananaTree = make(a(BananaTree,
             with(bananas, setOf(a(Banana), a(Banana), a(Banana)))));
     }
