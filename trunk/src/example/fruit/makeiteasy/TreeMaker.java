4cea3d2376006458d62d2044d7ff5043fe2d5ca5
nat.pryce <nat.pryce@37f576d0-09ce-11df-beaf-bb271b2086c6>
2010-02-01 16:47:33 +0000

    simplified code in Tree example

--- trunk/src/example/fruit/makeiteasy/TreeMaker.java
+++ trunk/src/example/fruit/makeiteasy/TreeMaker.java
@@ -1,9 +1,6 @@
 package example.fruit.makeiteasy;
 
 import com.natpryce.makeiteasy.Instantiator;
-import static com.natpryce.makeiteasy.MakeItEasy.a;
-import static com.natpryce.makeiteasy.MakeItEasy.an;
-import static com.natpryce.makeiteasy.MakeItEasy.make;
 import com.natpryce.makeiteasy.Property;
 import static com.natpryce.makeiteasy.Property.newProperty;
 import com.natpryce.makeiteasy.PropertyLookup;
@@ -11,19 +8,16 @@ import example.fruit.Apple;
 import example.fruit.Banana;
 import example.fruit.Fruit;
 import example.fruit.Tree;
-import static example.fruit.makeiteasy.FruitMakers.Apple;
-import static example.fruit.makeiteasy.FruitMakers.Banana;
 
-import static java.util.Arrays.asList;
-import java.util.List;
 import static java.util.Collections.emptyList;
+import java.util.List;
 
 
 /**
  * Java's broken type system makes it harder than it should be to define Properties and Instantiators
  * for generic types.
  * <p/>
- * To see the problem, inline the call to fruit() or emptyList() in the first instantiator below.
+ * To see the problem, inline the call to fruit() or emptyList() in the instantiator below.
  * As soon as the result of a generic method is passed as a parameter, instead of
  * assigned to a local variable or constant, the code stops compiling!
  * <p/>
@@ -49,21 +43,9 @@ public class TreeMaker {
         };
     }
 
+    public static final Instantiator<Tree<Apple>> AppleTree = Tree();
     public static final Property<Tree<Apple>, Iterable<? extends Apple>> apples = fruit();
-    public static final Instantiator<Tree<Apple>> AppleTree = new Instantiator<Tree<Apple>>() {
-        @Override
-        public Tree<Apple> instantiate(PropertyLookup<Tree<Apple>> lookup) {
-            Property<Tree<Apple>, Iterable<? extends Apple>> fruit = fruit();
 
-            return new Tree<Apple>(lookup.valueOf(apples, asList(make(an(Apple)))));
-        }
-    };
-
+    public static final Instantiator<Tree<Banana>> BananaTree = Tree();
     public static final Property<Tree<Banana>, Iterable<? extends Banana>> bananas = fruit();
-    public static final Instantiator<Tree<Banana>> BananaTree = new Instantiator<Tree<Banana>>() {
-        @Override
-        public Tree<Banana> instantiate(PropertyLookup<Tree<Banana>> lookup) {
-            return new Tree<Banana>(lookup.valueOf(bananas, asList(make(a(Banana)))));
-        }
-    };
 }
