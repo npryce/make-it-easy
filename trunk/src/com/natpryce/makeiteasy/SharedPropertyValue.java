fe984ed10c44d54679696895fb891299bafae397
nat.pryce <nat.pryce@37f576d0-09ce-11df-beaf-bb271b2086c6>
2010-05-13 22:08:47 +0000

    extracted PropertyValue API to an interface

---
+++ trunk/src/com/natpryce/makeiteasy/SharedPropertyValue.java
@@ -0,0 +1,33 @@
+package com.natpryce.makeiteasy;
+
+/**
+ * The value of a property that is shared among all instances created by a Maker.
+ *
+ * @param <T> the type of object that has the property
+ * @param <V> the type of the value of the property
+ */
+public class SharedPropertyValue<T,V> implements PropertyValue<T,V> {
+    private final Property<T,V> property;
+    private final V value;
+
+    public SharedPropertyValue(Property<T,V> property, V value) {
+        this.property = property;
+        this.value = value;
+    }
+
+    /**
+     * The property's value
+     */
+    @Override
+    public V value() {
+        return value;
+    }
+
+    /**
+     * The property
+     */
+    @Override
+    public Property<T, V> property() {
+        return property;
+    }
+}
